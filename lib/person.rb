910202a04064022d46a753b313e2f825fbd15992
Faith Janicek <faithe1937@gmail.com>
2019-10-30 21:29:08 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,28 @@
 # your code goes here
+class Person
+  attr_reader :name
+  attr_accessor :bank_account, :happiness, :hygiene
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+
+  def happiness
+    @happiness = 10 if @happiness > 10
+    @happiness = 0 if @happiness < 0
+    @happiness = happiness
+  end
+
+  def hygiene
+    @hygiene = 10 if @hygiene > 10
+    @hygiene = 0 if @hygiene < 0
+    @hygiene = hygiene
+  end
+  
+  def happy? 
+    if @happiness > 7 == false 
+    end 
+end 
